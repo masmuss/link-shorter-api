0e9110a1e96bd7e33553343307e5388204419595
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-11-20 10:04:44 +0700

    fix cors issue

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import router from './router/index.js'
 import db from './database/index.js'
+import cors from 'cors'
 
 const app = express()
+const corsOptions = {
+	origin: '*',
+	optionsSuccessStatus: 200,
+}
 
 app.use(express.json())
 	.use(router)
+	.use(cors(corsOptions))
 	.listen(process.env.PORT, () => {
 		console.log(`Listening at http://localhost:${process.env.PORT}`)
 	})
